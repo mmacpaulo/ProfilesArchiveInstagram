b7c718ab2ea76402f49a15f46651bcb298e27d79
cmpaulo <claudio.mpaulo@yahoo.com.br>
2022-03-20 16:29:27 -0300

    update full

--- WebscrapingInstagram.py
+++ WebscrapingInstagram.py
@@ -53,7 +53,7 @@ def buscar_palavras(keywords):
     import time
 
 #target the search input field
-    searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//input[@placeholder="Search"]')))
+    searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input')))
     
     searchbox.clear()
     # searchbox.send_keys(Keys.BACK_SPACE*50)
