6ce84f97b374b97dfb5f4d79174461055bcef83e
cmpaulo <claudio.mpaulo@yahoo.com.br>
2022-03-20 16:52:37 -0300

    update cut id xpath short print click

--- WebscrapingInstagram.py
+++ WebscrapingInstagram.py
@@ -53,9 +53,10 @@ def buscar_palavras(keywords):
     import time
 
 #target the search input field
-    searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]'))).click()
-    
+    searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]')))
     print(searchbox)
+    
+    searchbox.click()
     
     # searchbox.clear()
     searchbox.send_keys(Keys.BACK_SPACE*50)
