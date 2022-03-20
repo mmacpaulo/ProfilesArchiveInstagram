e2690c7226fee51d9cbb808919670b2fb06d20f8
cmpaulo <claudio.mpaulo@yahoo.com.br>
2022-03-20 13:54:12 -0300

    xpath button xpath searcher xpath cute

--- WebscrapingInstagram.py
+++ WebscrapingInstagram.py
@@ -53,7 +53,10 @@ def buscar_palavras(keywords):
     import time
 
 #target the search input field
-    searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')))
+    searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]')))
+    # searchbox.click()
+    # time.sleep(1)
+
     searchbox.clear()
 
 #search for the hashtag cat
