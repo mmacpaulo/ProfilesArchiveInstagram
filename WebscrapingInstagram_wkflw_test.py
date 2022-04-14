991850de4697eec40118610cbb2465d7184529b6
cmpaulo <claudio.mpaulo@yahoo.com.br>
2022-04-14 19:08:40 -0300

    try search some keys

--- WebscrapingInstagram_wkflw_test.py
+++ WebscrapingInstagram_wkflw_test.py
@@ -23,6 +23,12 @@ import os
 #specify the path to chromedriver.exe (download and save on your computer)
 option = webdriver.ChromeOptions()
 option.add_argument('headless')
+option.add_argument("--disable-gpu")
+option.add_argument("--window-size=1920,1200")
+option.add_argument("--ignore-certificate-errors")
+option.add_argument("--disable-extensions")
+option.add_argument("--no-sandbox")
+option.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
 
 #open the webpage
@@ -57,15 +63,17 @@ def buscar_palavras(keywords):
     import time
 
 #target the search input field
-    searchbox = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')))
-    searchbox.clear()
+    searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]')))
+    entrada = searchbox.find_element(By.TAG_NAME,'input')
+    entrada.clear()
 
 
 #search for the hashtag cat
     # keyword = "arquivologia"
-    searchbox.send_keys(keywords)
+    entrada.send_keys(keywords)
     time.sleep(2)
-    divs = driver.find_elements(By.CLASS_NAME, 'fuqBx')
+    divs = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located(By.CLASS_NAME, 'fuqBx'))
+    # divs = driver.find_elements(By.CLASS_NAME, 'fuqBx')
 
     return divs
 
