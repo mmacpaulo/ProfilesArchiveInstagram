683b8e7829a06c0b498eb93f06e746d1482946aa
cmpaulo <claudio.mpaulo@yahoo.com.br>
2022-04-14 18:35:23 -0300

    repair bot auto search

--- WebscrapingInstagram_wkflw_test.py
+++ WebscrapingInstagram_wkflw_test.py
@@ -57,7 +57,7 @@ def buscar_palavras(keywords):
     import time
 
 #target the search input field
-    searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Search']")))
+    searchbox = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')))
     searchbox.clear()
 
 
