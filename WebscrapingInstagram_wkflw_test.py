cbe30467086b255245ce4f74fea834aad25d0ea4
cmpaulo <claudio.mpaulo@yahoo.com.br>
2022-04-14 21:21:31 -0300

    ftry spath method

--- WebscrapingInstagram_wkflw_test.py
+++ WebscrapingInstagram_wkflw_test.py
@@ -30,6 +30,7 @@ option.add_argument("--disable-extensions")
 # option.add_argument("--no-sandbox")
 option.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
+driver.implicitly_wait(10)
 
 #open the webpage
 driver.get("http://www.instagram.com")
@@ -71,10 +72,10 @@ def buscar_palavras(keywords):
 #search for the hashtag cat
     # keyword = "arquivologia"
     entrada.send_keys(keywords)
-    time.sleep(10)
+    time.sleep(5)
     # _01UL2 fuqBx
-    # divs = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, 'yPP5B')))
-    divs = driver.find_elements(By.CLASS_NAME, 'fuqBx')
+    popUp = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]')))
+    divs = popUp.find_elements(By.CLASS_NAME, 'fuqBx')
 
     return divs
 
